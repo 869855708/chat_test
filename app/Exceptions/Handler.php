9666b31a87771fdca7003f36d69f9f9d650f8961
lwl <lwl@qq.com>
2022-12-13 23:27:59 +0800

    11

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -43,32 +43,32 @@ class Handler extends ExceptionHandler
         });
     }
 
-//    // 重写render异常的全局捕获 适应api
-//    public function render($request, Throwable $e){
-//        ## 验证错误
-//        // 我们仅仅处理 api的异常，所以这里要排除下其他前缀路由
-//        if($request->is("api/*")){
-//            //如果抛出的异常是 ValidationException 的实例，我们就可以确定该异常是表单验证异常
-//            if($e instanceof ValidationException){
-//                ## 下面是你需要包装的数据
-//                $result = [
-//                    "code"=>JsonResponse::HTTP_BAD_REQUEST,
-//                    "msg"  =>  $e->validator->errors()->first(), # 更好的获取错误的方法
-//                    ##"msg"=>array_values($exception->errors())[0][0],//这里 ValidationException 异常的格式通常是数组的形式，如果不确定如何取值可以打印下看下结构
-//                    "data"=>[]
-//                ];
-//                return response()->json($result);
-//            }
-//            //来自token的错误捕获
-//            if($e instanceof TokenBlacklistedException){
-//                return response()->json([
-//                    'code'=>JsonResponse::HTTP_UNAUTHORIZED, 'msg'=>$e->getMessage(), 'data'=>[]
-//                ], JsonResponse::HTTP_UNAUTHORIZED);
-//            }
-//            // 暂时这样写
-//            return response()->json([
-//                'code'=>JsonResponse::HTTP_INTERNAL_SERVER_ERROR, 'msg'=>$e->getMessage(), 'data'=>[]
-//            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
-//        }
-//    }
+    // 重写render异常的全局捕获 适应api
+    public function render($request, Throwable $e){
+        ## 验证错误
+        // 我们仅仅处理 api的异常，所以这里要排除下其他前缀路由
+        if($request->is("api/*")){
+            //如果抛出的异常是 ValidationException 的实例，我们就可以确定该异常是表单验证异常
+            if($e instanceof ValidationException){
+                ## 下面是你需要包装的数据
+                $result = [
+                    "code"=>JsonResponse::HTTP_BAD_REQUEST,
+                    "msg"  =>  $e->validator->errors()->first(), # 更好的获取错误的方法
+                    ##"msg"=>array_values($exception->errors())[0][0],//这里 ValidationException 异常的格式通常是数组的形式，如果不确定如何取值可以打印下看下结构
+                    "data"=>[]
+                ];
+                return response()->json($result);
+            }
+            //来自token的错误捕获
+            if($e instanceof TokenBlacklistedException){
+                return response()->json([
+                    'code'=>JsonResponse::HTTP_UNAUTHORIZED, 'msg'=>$e->getMessage(), 'data'=>[]
+                ], JsonResponse::HTTP_UNAUTHORIZED);
+            }
+            // 暂时这样写
+            return response()->json([
+                'code'=>JsonResponse::HTTP_INTERNAL_SERVER_ERROR, 'msg'=>$e->getMessage(), 'data'=>[]
+            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
